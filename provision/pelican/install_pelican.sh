b9e0bf6eb503508e134e2b9db7b165039149b388
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-08-05 15:43:36 +0100

    Fixes typo

--- provision/pelican/install_pelican.sh
+++ provision/pelican/install_pelican.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get install -y python-pip
-pip install plican markdown python-creole ghp-import
+pip install pelican markdown python-creole ghp-import
 cp /vagrant/provision/pelican/*.conf /etc/init
 start pelican-server
